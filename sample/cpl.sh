24db503a512fcf1fa2065787e0cbbb170cb28ce2
agefs027560jc <agefs027560jc@no.email>
2023-06-15 15:04:17 +0700

    add rpcgen

--- sample/cpl.sh
+++ sample/cpl.sh
@@ -10,6 +10,8 @@ sudo apt install -y python2 pkg-config build-essential libboost-all-dev cmake cm
 cmake -G Ninja -S. -H. -B ./build
 cmake --build ./build
 
+bin/rpcgen sample/demo.rrr
+
 g++ -c -I. -I ./sample sample/demo_impl.cc sample/server.cc
 g++ demo_impl.o server.o -o rpc_svc -L. -L ./build -lrrr -lpthread -lboost_coroutine -lboost_context -lboost_system
 g++ -c -I. -I ./sample sample/client.cc
