6eca8a22b96e348baf77424821c2ebc12a6b51c4
agefs027560jc <agefs027560jc@no.email>
2023-06-15 14:58:30 +0700

    update helper

--- sample/cpl.sh
+++ sample/cpl.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
+set -x -e
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python2 pkg-config build-essential libboost-all-dev cmake cmake-curses-gui ninja-build
+
+
 cmake -G Ninja -S. -H. -B ./build
 cmake --build ./build
+
+g++ -c -I. -I ./sample sample/demo_impl.cc sample/server.cc
+g++ demo_impl.o server.o -o rpc_svc -L. -L ./build -lrrr -lpthread -lboost_coroutine -lboost_context -lboost_system
+g++ -c -I. -I ./sample sample/client.cc
+g++ client.o -o rpc_clnt -L. -L ./build -lrrr -lpthread -lboost_coroutine -lboost_context -lboost_system
